95968cd3ec01ccd302cb76552b1aac9acae86302
pablo.santos <pablo.santos@compline.com.br>
2021-07-27 21:31:19 -0300

    Ajustes no application

--- restapi-leads-managemet/src/main/java/br/com/leadsmanagement/RestapiLeadsManagemetApplication.java
+++ restapi-leads-managemet/src/main/java/br/com/leadsmanagement/RestapiLeadsManagemetApplication.java
@@ -27,12 +27,7 @@ import br.com.leadsmanagement.config.Env;
 import br.com.leadsmanagement.mongodb.MongoConfiguration;
 import br.com.leadsmanagement.mongodb.MongoProperties;
 
-//@EnableWebFlux
 @SpringBootApplication
-//@AutoConfigureAfter(EmbeddedMongoAutoConfiguration.class)
-//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
-//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoRepositoriesAutoConfiguration .class,MongoDataAutoConfiguration.class})
-//@EnableReactiveMongoRepositories
 public class RestapiLeadsManagemetApplication {
 
 	public static void main(String[] args) {
@@ -41,39 +36,13 @@ public class RestapiLeadsManagemetApplication {
 		Locale.setDefault(new Locale("pt", "BR"));
 		System.setProperty("file.encoding", "UTF-8");
 		
-		//MongoProperties mongoProperties = new MongoProperties("mongodb://UsrShippetsApi:shippets.2021@rshom-shard-00-01.zhdgo.mongodb.net:27017,rshom-shard-00-00.zhdgo.mongodb.net:27017,rshom-shard-00-02.zhdgo.mongodb.net:27017/db_shippets_service_api");
-		
 		SpringApplication.run(RestapiLeadsManagemetApplication.class, args);
 	}
-	
-	
 	
 	@Bean
 	@Scope("singleton")
 	public Env env() {
 		return Env.getInstance();
 	}
-	
-	/*
-	  @Override protected String getDatabaseName() { 
-		  // TODO Auto-generated method stub 
-		  return Env.getInstance().getMongoConfig().getDbName(); 
-	  }
-	  
-	  @Override
-	  @Bean 
-	  public MongoClient reactiveMongoClient() {
-		  // TODO Auto-generated method stub 
-		  MongoClient mongoClient = MongoClients.create(MongoClientSettings.builder()
-				  .applyConnectionString( new ConnectionString("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false")) 
-				  .streamFactoryFactory(NettyStreamFactoryFactory.builder().build())
-				  .build());
-	  
-		  return mongoClient; 
-	  
-	  }*/
-	  
-	 
-	 
 	
 }
